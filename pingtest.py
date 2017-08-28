dcda2e59261b5403c388978edb65890095f01374
16222010168 <cyc870528@hotmail.com>
2017-08-28 10:43:30 +0800

    Update pingtest.py

--- pingtest.py
+++ pingtest.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
+"""
+This is a ping test script, it helps to check the connetion of the multiple remote system!
+"""
+
 import subprocess
 ping = 'ping'
 arg = '-c'
 count ='5'
-l= ['10.1.1.206','192.168.1.1','10.100.8.106','10.100.8.107']
+l= ['10.1.1.206','192.168.1.1','10.100.8.106','10.100.8.107']   #Input ip address of the system you want to check to the list.
 for ipaddr in l:
         a = subprocess.call([ping,arg,count,ipaddr])
         if a == 0:
