54566f8882684e13746fd7988d373a1bd62435fa
16222010168 <cyc870528@hotmail.com>
2017-08-28 16:06:49 +0800

    Update ftp.py

--- ftp.py
+++ ftp.py
@@ -3,8 +3,7 @@ import pexpect
 
 account = {'10.100.8.107':'yunwei','10.100.8.108':'yunwei','10.100.8.106':'yunwei','10.1.1.206':'root','10.1.1.211':'root','10.1.1.204':'root','10.1.1.210':'root','10.100.8.49':'root'}
 ywpasswd = {'10.100.8.107':'1qaz2wsx','10.100.8.108':'1qaz2wsx','10.100.8.106':'1qaz2wsx'}
-rootpasswd = {'10.1.1.206':'1qaz&ujm','10.1.1.211':'1qaz@WSX','10.1.1.204':'qp0512sap','10.1.1.210':'1qaz@WSX','10.100.8.49':
-'root'}
+rootpasswd = {'10.1.1.206':'1qaz&ujm','10.1.1.211':'1qaz@WSX','10.1.1.204':'qp0512sap','10.1.1.210':'1qaz@WSX','10.100.8.49':'root'}
 ipaddr = account.keys()
 source = raw_input('Source Path:')
 destination = raw_input('destination path:')
